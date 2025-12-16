639636c756a2c47925226ec562306c88d93a65ad
Felipe Inoue <felipeinoue@yahoo.com.br>
2025-12-16 18:17:09 -0300

    add open cfg file for windows

--- internal/configuration/open.go
+++ internal/configuration/open.go
@@ -2,14 +2,24 @@ package configuration
 
 import (
 	"fmt"
+	"runtime"
 
 	"github.com/fiwon123/crower/internal/data"
 	"github.com/fiwon123/crower/internal/handlers"
 )
 
 func Open(cfgFilePath string) {
-	handlers.Execute(*data.NewCommand(
-		"open",
-		[]string{},
-		fmt.Sprintf("xdg-open %s", cfgFilePath)))
+	switch runtime.GOOS {
+	case "windows":
+		handlers.Execute(*data.NewCommand(
+			"open",
+			[]string{},
+			fmt.Sprintf("start %s", cfgFilePath)))
+	case "linux":
+		handlers.Execute(*data.NewCommand(
+			"open",
+			[]string{},
+			fmt.Sprintf("xdg-open %s", cfgFilePath)))
+	}
+
 }
