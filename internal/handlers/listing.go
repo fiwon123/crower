6b5e73b6337678f61a655c26eac7b300ff85a864
Felipe Inoue <felipeinoue@yahoo.com.br>
2025-12-21 19:05:18 -0300

    update listing

--- internal/handlers/listing.go
+++ internal/handlers/listing.go
@@ -2,22 +2,20 @@ package handlers
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/fiwon123/crower/internal/data"
 )
 
 func List(app *data.App) {
-	fmt.Println("list all commands:")
-	fmt.Println("-----------------------------")
+	fmt.Println("------------------------------------------------")
 	printMap(app.AllCommandsByName)
 }
 
 func printMap(m map[string]data.Command) {
-	for k, command := range m {
-		fmt.Printf("[%s] \n", k)
-		fmt.Printf("  Name    = %s \n", command.Name)
-		fmt.Printf("  Aliases = %s \n", command.AllAlias)
-		fmt.Printf("  Exec    = %s \n", command.Exec)
-		fmt.Println("")
+	fmt.Printf("ID %-12s %-16s %-8s \n", "Name", "Aliases", "Exec")
+	fmt.Println("------------------------------------------------")
+	for _, command := range m {
+		fmt.Printf("%-12s %-16v %-8s \n", command.Name, strings.Join(command.AllAlias, ","), command.Exec)
 	}
 }
