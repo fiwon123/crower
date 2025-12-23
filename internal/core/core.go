e471fb7a6ed7df1270ad39c080fa4d8440a8eab6
Felipe Inoue <felipeinoue@yahoo.com.br>
2025-12-22 23:02:22 -0300

    refactor core.go

--- internal/core/core.go
+++ internal/core/core.go
@@ -39,80 +39,120 @@ func InitApp(cfgFilePath string) *data.App {
 func HandlePayload(payload data.Payload, app *data.App) {
 	switch payload.Op {
 	case data.ExecuteOp:
-		output, err := handlers.Execute(payload.Name, payload.Args, app)
-		if err != nil {
-			app.LoggerInfo.Error("Error trying to run command: ", string(output), err)
-			return
-		}
-		fmt.Println(string(output))
+		executeOp(payload, app)
 	case data.AddOp:
-		err := handlers.AddCommand(payload.Name, payload.Alias, payload.Exec, payload.Args, app)
-
-		if err == nil {
-			utils.WriteToml(app.AllCommandsByName, app.CfgFilePath)
-			app.LoggerInfo.Info("added new command: ", app.AllCommandsByName)
-
-			app.History.Add("Add")
-			handlers.SaveHistory(app)
-		} else {
-			app.LoggerInfo.Error("Error add command: ", err, payload)
-		}
+		addOp(payload, app)
 	case data.AddProcess:
-		err := handlers.AddProcess(payload.Name, payload.Args, app)
-		if err == nil {
-			utils.WriteToml(app.AllCommandsByName, app.CfgFilePath)
-			app.LoggerInfo.Info("added new command by process: ", app.AllCommandsByName)
-
-			app.History.Add("Add Process")
-			handlers.SaveHistory(app)
-		} else {
-			app.LoggerInfo.Error("Error add command by process: ", err, payload)
-		}
+		addProcess(payload, app)
 	case data.DeleteOp:
-		if handlers.DeleteCommand(payload.Name, app) {
-			app.LoggerInfo.Info("deleted command: ", app.AllCommandsByName)
-			utils.WriteToml(app.AllCommandsByName, app.CfgFilePath)
-
-			app.History.Add("Delete")
-			handlers.SaveHistory(app)
-		} else {
-			app.LoggerInfo.Error("Error delete command: ", payload)
-		}
+		deleteOp(payload, app)
 	case data.UpdateOp:
-		key := ""
-		if len(payload.Args) != 0 {
-			key = payload.Args[0]
-		}
-		err := checkInputUpdate(&key, &payload.Name, &payload.Alias, &payload.Exec, app)
-		if err != nil {
-			app.LoggerInfo.Error("Error update command: ", err, payload)
-		}
-
-		err = handlers.UpdateCommand(key, payload.Name, payload.Alias, payload.Exec, app)
-		if err == nil {
-			app.LoggerInfo.Info("updated command: ", app.AllCommandsByName)
-			utils.WriteToml(app.AllCommandsByName, app.CfgFilePath)
-
-			app.History.Add("Update")
-			handlers.SaveHistory(app)
-		} else {
-			app.LoggerInfo.Error("Error update command: ", err, payload)
-		}
+		updateOp(payload, app)
 	case data.ListOp:
-		handlers.List(app)
+		listOp(app)
 	case data.ResetOp:
-		handlers.Reset(app)
+		resetOp(app)
+	case data.OpenOp:
+		openOp(app)
+	case data.ProcessOp:
+		processOp(payload, app)
+	case data.HistoryOp:
+		historyOp(app)
+	}
+}
+
+func historyOp(app *data.App) {
+	handlers.History(app)
+}
+
+func processOp(payload data.Payload, app *data.App) {
+	handlers.Process(payload.Args, app)
+}
+
+func openOp(app *data.App) {
+	handlers.Open(app.CfgFilePath, app)
+}
+
+func resetOp(app *data.App) {
+	handlers.Reset(app)
+	utils.WriteToml(app.AllCommandsByName, app.CfgFilePath)
+
+	app.History.Add("Reset")
+	handlers.SaveHistory(app)
+
+	app.LoggerInfo.Info("reset all commands: ", app.AllCommandsByName)
+}
+
+func listOp(app *data.App) {
+	handlers.List(app)
+}
+
+func updateOp(payload data.Payload, app *data.App) {
+	key := ""
+	if len(payload.Args) != 0 {
+		key = payload.Args[0]
+	}
+	err := checkInputUpdate(&key, &payload.Name, &payload.Alias, &payload.Exec, app)
+	if err != nil {
+		app.LoggerInfo.Error("Error update command: ", err, payload)
+	}
+
+	err = handlers.UpdateCommand(key, payload.Name, payload.Alias, payload.Exec, app)
+	if err == nil {
+		app.LoggerInfo.Info("updated command: ", app.AllCommandsByName)
 		utils.WriteToml(app.AllCommandsByName, app.CfgFilePath)
 
-		app.History.Add("Reset")
+		app.History.Add("Update")
 		handlers.SaveHistory(app)
-
-		app.LoggerInfo.Info("reset all commands: ", app.AllCommandsByName)
-	case data.OpenOp:
-		handlers.Open(app.CfgFilePath, app)
-	case data.ProcessOp:
-		handlers.Process(payload.Args, app)
-	case data.HistoryOp:
-		handlers.History(app)
+	} else {
+		app.LoggerInfo.Error("Error update command: ", err, payload)
 	}
+}
+
+func deleteOp(payload data.Payload, app *data.App) {
+	if handlers.DeleteCommand(payload.Name, app) {
+		app.LoggerInfo.Info("deleted command: ", app.AllCommandsByName)
+		utils.WriteToml(app.AllCommandsByName, app.CfgFilePath)
+
+		app.History.Add("Delete")
+		handlers.SaveHistory(app)
+	} else {
+		app.LoggerInfo.Error("Error delete command: ", payload)
+	}
+}
+
+func addProcess(payload data.Payload, app *data.App) {
+	err := handlers.AddProcess(payload.Name, payload.Args, app)
+	if err == nil {
+		utils.WriteToml(app.AllCommandsByName, app.CfgFilePath)
+		app.LoggerInfo.Info("added new command by process: ", app.AllCommandsByName)
+
+		app.History.Add("Add Process")
+		handlers.SaveHistory(app)
+	} else {
+		app.LoggerInfo.Error("Error add command by process: ", err, payload)
+	}
+}
+
+func addOp(payload data.Payload, app *data.App) {
+	err := handlers.AddCommand(payload.Name, payload.Alias, payload.Exec, payload.Args, app)
+
+	if err == nil {
+		utils.WriteToml(app.AllCommandsByName, app.CfgFilePath)
+		app.LoggerInfo.Info("added new command: ", app.AllCommandsByName)
+
+		app.History.Add("Add")
+		handlers.SaveHistory(app)
+	} else {
+		app.LoggerInfo.Error("Error add command: ", err, payload)
+	}
+}
+
+func executeOp(payload data.Payload, app *data.App) {
+	output, err := handlers.Execute(payload.Name, payload.Args, app)
+	if err != nil {
+		app.LoggerInfo.Error("Error trying to run command: ", string(output), err)
+		return
+	}
+	fmt.Println(string(output))
 }
