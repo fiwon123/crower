072c6680a54d932919497d3e5a859ef41ccff428
Your <you@example.com>
2025-12-29 17:39:04 -0300

    implement log for move cmd

--- internal/handlers/move.go
+++ internal/handlers/move.go
@@ -8,21 +8,38 @@ import (
 )
 
 func MoveFile(filePath string, destFolder string, app *app.Data) {
+	var out []byte
+	var err error
 	switch runtime.GOOS {
 	case "windows":
-		PerformExecute(fmt.Sprintf("move '%s' '%s'", filePath, destFolder))
+		out, err = PerformExecute(fmt.Sprintf("move '%s' '%s'", filePath, destFolder))
 	case "linux":
-		PerformExecute(fmt.Sprintf("mv '%s' '%s'", filePath, destFolder))
+		out, err = PerformExecute(fmt.Sprintf("mv '%s' '%s'", filePath, destFolder))
 	}
 
+	if err != nil {
+		fmt.Printf("out %s, error %v\n", out, err)
+		return
+	}
+
+	fmt.Println("result: ", string(out))
 }
 
 func MoveFolder(folderPath string, destFolder string, app *app.Data) {
+	var out []byte
+	var err error
 	switch runtime.GOOS {
 	case "windows":
-		PerformExecute(fmt.Sprintf("move '%s' '%s'", folderPath, destFolder))
+		out, err = PerformExecute(fmt.Sprintf("move '%s' '%s'", folderPath, destFolder))
 	case "linux":
-		PerformExecute(fmt.Sprintf("mv '%s' '%s'", folderPath, destFolder))
+		out, err = PerformExecute(fmt.Sprintf("mv '%s' '%s'", folderPath, destFolder))
 	}
+
+	if err != nil {
+		fmt.Printf("out %s, error %v\n", out, err)
+		return
+	}
+
+	fmt.Println("result: ", string(out))
 
 }
