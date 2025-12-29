0168665881c04c52d70798e3f4467a95f321a459
Your <you@example.com>
2025-12-29 17:37:40 -0300

    implement logs for copy cmd

--- internal/handlers/copy.go
+++ internal/handlers/copy.go
@@ -8,21 +8,38 @@ import (
 )
 
 func CopyFile(filePath string, destFolder string, app *app.Data) {
+	var out []byte
+	var err error
 	switch runtime.GOOS {
 	case "windows":
-		PerformExecute(fmt.Sprintf("'copy %s %s'", filePath, destFolder))
+		out, err = PerformExecute(fmt.Sprintf("'copy %s %s'", filePath, destFolder))
 	case "linux":
-		PerformExecute(fmt.Sprintf("'cp %s %s'", filePath, destFolder))
+		out, err = PerformExecute(fmt.Sprintf("'cp %s %s'", filePath, destFolder))
 	}
+
+	if err != nil {
+		fmt.Printf("out %s, error %v\n", out, err)
+		return
+	}
+
+	fmt.Println("result: ", string(out))
 
 }
 
 func CopyFolder(filePath string, destFolder string, app *app.Data) {
+	var out []byte
+	var err error
 	switch runtime.GOOS {
 	case "windows":
-		PerformExecute(fmt.Sprintf("'xcopy %s %s /E /I'", filePath, destFolder))
+		out, err = PerformExecute(fmt.Sprintf("'xcopy %s %s /E /I'", filePath, destFolder))
 	case "linux":
-		PerformExecute(fmt.Sprintf("'cp -r %s %s'", filePath, destFolder))
+		out, err = PerformExecute(fmt.Sprintf("'cp -r %s %s'", filePath, destFolder))
 	}
 
+	if err != nil {
+		fmt.Printf("out %s, error %v\n", out, err)
+		return
+	}
+
+	fmt.Println("result: ", string(out))
 }
