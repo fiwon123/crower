cf80e27954892e693e06ff978427f240bb9e6e3f
Your <you@example.com>
2025-12-29 16:01:04 -0300

    update extract cmd text

--- cmd/extract/extract.go
+++ cmd/extract/extract.go
@@ -13,7 +13,19 @@ var outDirFlag string
 var Cmd = &cobra.Command{
 	Use:   "extract",
 	Short: "extract compressed files (.zip, .tar, .tar.gz, .7z, .rar...)",
-	Long:  `extract compressed files (.zip, .tar, .tar.gz, .7z, .rar...)`,
+	Long: `extract compressed files (.zip, .tar, .tar.gz, .7z, .rar...)
+
+Type 'crower/cr extract "PATH_TO_FILE"' to extract all content to the current folder
+You can specify out to change output folder: 'crower/cr extract "COMPRESSED_FILE_PATH" --out/-o "OUTPUT_FOLDER_PATH"'
+If you have more than 1 compressed file to extract you can put * as the name file
+
+Examples:
+	crower extract "C:\Users\Test\Desktop\Test\files.zip"
+	crower extract "C:\Users\Test\Desktop\Test\files.zip" -o "C:\Users\Test\Desktop\Out\"
+	crower extract "C:\Users\Test\Desktop\Test\*.zip"
+	crower extract "C:\Users\Test\Desktop\Test\*.zip" -o "C:\Users\Test\Desktop\Out\"
+
+	`,
 	Run: func(cmd *cobra.Command, args []string) {
 		cfgFilePath, _ := cmdsHelper.GetPersistentConfigFlag(cmd)
 
